a59cc1e17915095b4081d1a4dd445f287d4ee863
Collop1 <collop111cop@gmail.com>
2025-08-16 15:45:55 +1200

    Add lab4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
 import Greeting from './components/Greeting.jsx'
-import SingleCat from './components/SingleCat.jsx'
-import cats from './components/BigCats.jsx'
+import { EditedCats } from './components/BigCats.jsx'
 import MoodChanger from './components/Emoji.jsx'
 
 function App() {
-
   return (
     <>
       <h1><Greeting /></h1>
@@ -14,11 +12,7 @@ function App() {
       <h2>Mood changer</h2>
       <MoodChanger />
       <h2>Big Cats</h2>
-      <div>
-        {cats.map((cat) => (
-          <SingleCat key={cat.id} cat={cat} />
-        ))}
-      </div>
+      <EditedCats />
     </>
   )
 }
--- src/components/BigCats.jsx
+++ src/components/BigCats.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import SingleCat from './SingleCat.jsx';
 import cheetahImg from '../assets/Cheetah.png';
 import cougarImg from '../assets/Cougar.png';
 import jaguarImg from '../assets/Jaguar.png';
@@ -6,14 +8,90 @@ import lionImg from '../assets/Lion.png';
 import snowLeopardImg from '../assets/Snow leopard.png';
 import tigerImg from '../assets/Tiger.png';
 
-const cats = [ 
-    { id: 1, name: 'Cheetah', latinName: 'Acinonyx jubatus', image: cheetahImg }, 
-    { id: 2, name: 'Cougar', latinName: 'Puma concolor', image: cougarImg }, 
-    { id: 3, name: 'Jaguar', latinName: 'Panthera onca', image: jaguarImg }, 
-    { id: 4, name: 'Leopard', latinName: 'Panthera pardus', image: leopardImg }, 
-    { id: 5, name: 'Lion', latinName: 'Panthera leo', image: lionImg }, 
-    { id: 6, name: 'Snow leopard', latinName: 'Panthera uncia', image: snowLeopardImg }, 
-    { id: 7, name: 'Tiger', latinName: 'Panthera tigris', image: tigerImg },
+const initialCats = [ 
+    { id: 1, name: 'Lion', latinName: 'Panthera leo', image: lionImg },
+    { id: 2, name: 'Cougar', latinName: 'Puma concolor', image: cougarImg },
+    { id: 3, name: 'Snow leopard', latinName: 'Panthera uncia', image: snowLeopardImg },
+    { id: 4, name: 'Cheetah', latinName: 'Acinonyx jubatus', image: cheetahImg },
+    { id: 5, name: 'Tiger', latinName: 'Panthera tigris', image: tigerImg },
+    { id: 6, name: 'Jaguar', latinName: 'Panthera onca', image: jaguarImg },
+    { id: 7, name: 'Leopard', latinName: 'Panthera pardus', image: leopardImg }
 ]
 
-export default cats;
+function Sort({ onSortAlphabetically, onReverse }) {
+    return (
+        <div>
+            <button onClick={onSortAlphabetically}>Sort Alphabetically</button>
+            <button onClick={onReverse}>Reverse list</button>
+        </div>
+    );
+}
+
+function Filter({ isFiltered, onFilterToggle }) {
+    return (
+        <button onClick={onFilterToggle}>
+            {isFiltered ? 'Show All' : 'Show Pantheras'}
+        </button>
+    );
+}
+
+// Combine sorting and filtering to one list
+function EditedCats() {
+    const [isFiltered, setIsFiltered] = useState(false);
+    const [sortState, setSortState] = useState('none');
+    const [displayedCats, setDisplayedCats] = useState(initialCats);
+    
+    const applyFilterAndSort = (cats, filterStatus, sortType) => {
+        let result = [...cats];
+        
+        if (filterStatus) {
+            result = result.filter(cat => cat.latinName.startsWith('Panthera'));
+        }
+        
+        if (sortType === 'alphabetical') {
+            result = result.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (sortType === 'reverse') {
+            result = result.sort((a, b) => a.name.localeCompare(b.name)).reverse();
+        }
+        
+        return result;
+    };
+    
+    const handleSortAlphabetically = () => {
+        setSortState('alphabetical');
+        setDisplayedCats(applyFilterAndSort(initialCats, isFiltered, 'alphabetical'));
+    };
+    
+    const handleReverse = () => {
+        setSortState('reverse');
+        setDisplayedCats(applyFilterAndSort(initialCats, isFiltered, 'reverse'));
+    };
+    
+    const handleFilterToggle = () => {
+        const newFilterState = !isFiltered;
+        setIsFiltered(newFilterState);
+        setDisplayedCats(applyFilterAndSort(initialCats, newFilterState, sortState));
+    };
+    
+    return (
+        <div>
+            <div className="controls">
+                <Sort 
+                    onSortAlphabetically={handleSortAlphabetically} 
+                    onReverse={handleReverse} 
+                />
+                <Filter 
+                    isFiltered={isFiltered} 
+                    onFilterToggle={handleFilterToggle} 
+                />
+            </div>
+            <div className="cats-container">
+                {displayedCats.map((cat) => (
+                    <SingleCat key={cat.id} cat={cat} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export { initialCats, EditedCats };
